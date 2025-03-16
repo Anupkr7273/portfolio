50cc6334f7be5fb3719e5cd8ecae4c8d57f079f6
Anup kumar <anupkr72732gamil.com>
2025-03-16 16:22:06 +0530

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="py-4 text-center bg-gray-900 text-gray-400 shadow-lg shadow-gray-700 rounded-t-2xl ">
-            <p className="text-lg ">&copy; {new Date().getFullYear()} Anup Kumar. All rights reserved.</p>
+            <p className="text-sm ">&copy; {new Date().getFullYear()} Anup Kumar. All rights reserved.</p>
         </footer>
     );
 };
